fix bug in weight sign monitoring

main.py
@@ -26,8 +26,9 @@ def epoch(epoch_num, loader,  model, opt, writer, config):
         y = y.to(config['device'])
         out = model.forward(x)
 
+        # Calc loss function
         all_params = model.get_flattened_params()
-        loss = F.cross_entropy(out, y) + all_params.norm(p=2)*config['wdecay'] 
+        loss = F.cross_entropy(out, y) + all_params.norm(p=1)*config['wdecay'] 
 
         if model.training:            
             model.save_weights()
@@ -46,6 +47,10 @@ def epoch(epoch_num, loader,  model, opt, writer, config):
             total_remaining, remaining_pos = model.get_sign_percentages()
 
             writer.add_scalar('signs/remaining_pos', remaining_pos/total_remaining, update_num)
+            writer.add_scalar('signs/remaining_total', total_remaining, update_num)
+            writer.add_scalar('signs/reamining_pos_absolute', remaining_pos, update_num)
+
+
             writer.add_scalar('sparsity/sparsity_after_step', model.get_sparsity(config), update_num)
             # Monitor wegiths for flips
             flips_since_last = model.store_flips_since_last()

master_model.py
@@ -196,7 +196,9 @@ class MasterModel(nn.Module):
         with torch.no_grad():
             for layer in self.parameters():
                 flat_layer = layer.flatten()
-                total_remaining_weights += flat_layer.numel()
-                remaining_pos += flat_layer[flat_layer > 0].numel()
+                remaining = flat_layer[flat_layer != 0]
+
+                total_remaining_weights += remaining.numel()
+                remaining_pos += remaining[remaining > 0].numel()
     
         return total_remaining_weights, remaining_pos