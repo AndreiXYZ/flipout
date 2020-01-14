7e0a13d5d3d90ab20cc088ce0a9daf24c84193ec
Andrei Apostol <apo.andrei23@yahoo.com>
2020-01-14 14:46:38 +0100

    fix typo in LinearMasked

--- layers.py
+++ layers.py
@@ -10,7 +10,7 @@ class LinearMasked(nn.Module):
         self.input_features = input_features
         self.output_features = output_features
 
-        self.weights = nn.Parameter(torch.Tensor(output_features, input_features))
+        self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
 
         if 'bias':
             self.bias = nn.Parameter(torch.Tensor(output_features))
@@ -20,19 +20,19 @@ class LinearMasked(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5))
-        self.weight_signs = self.weights.clone().detach().sign().to('cuda')
-        self.weights.data.abs_()
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        self.weight_signs = self.weight.clone().detach().sign().to('cuda')
+        self.weight.data.abs_()
 
         if self.bias is not None:
-            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weights)
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
             bound = 1 / math.sqrt(fan_in)
             nn.init.uniform_(self.bias, -bound, bound)
             self.bias_signs = self.bias.clone().detach().sign().to('cuda')
             self.bias.data.abs_()
             
     def forward(self, input):
-        signed_weights = F.relu(self.weights)*self.weight_signs
+        signed_weights = F.relu(self.weight)*self.weight_signs
         
         if self.bias is not None:
             signed_bias = F.relu(self.bias)*self.bias_signs
