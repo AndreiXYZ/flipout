cf45eaf5adfbb3ff43e64ca4bc6a0ea9fe67a656
Andrei Apostol <apo.andrei23@yahoo.com>
2020-01-17 14:31:13 +0100

    remove unnecessary construct_run_name

--- utils.py
+++ utils.py
@@ -10,9 +10,6 @@ def set_seed(seed):
     torch.backends.cudnn.benchmark = False
     np.random.seed(seed)
 
-def construct_run_name(config):
-    return ''.join(['_'+str(key)+'_'+str(value) if key!='comment' else '' for key,value in config.items()])
-
 def get_opt(config, model):
     params = model.parameters()
     lr = config['lr']
@@ -55,8 +52,7 @@ def plot_stats(train_acc, train_loss, test_acc, test_loss, model, writer, epoch_
         writer.add_scalar('loss/train', train_loss, epoch_num)
         writer.add_scalar('loss/test', test_loss, epoch_num)
         writer.add_scalar('sparsity/sparsity', model.get_sparsity(config), epoch_num)
-
-
+    
 def print_gc_memory_usage():
     total_usage = 0
     for obj in gc.get_objects():
