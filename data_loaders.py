4006f2cb8b6860ecdfa2e490ab5d9525dfb6b5ac
Andrei Apostol <apo.andrei23@yahoo.com>
2020-01-24 12:25:26 +0100

    add some transforms to cifar10 loaders

--- data_loaders.py
+++ data_loaders.py
@@ -29,14 +29,21 @@ def get_mnist_loaders(config):
 
 
 def get_cifar10_loaders(config):
-    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                                 std=[0.5, 0.5, 0.5])
+    transform_train = transforms.Compose([
+                        transforms.RandomCrop(32, padding=4),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
     
-    train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.Compose([transforms.ToTensor(),
-                                                                                                        normalize]))
-    test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms.Compose([transforms.ToTensor(),
-                                                                                                        normalize]))
+    transform_test = transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
+    train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.Compose(transform_train)
+    test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms.Compose(transform_test)
+    
     train_loader = DataLoader(train_set,
                               batch_size = config['batch_size'],
                               shuffle = True,
