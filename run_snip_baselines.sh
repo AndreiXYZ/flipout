482ca0d12ef089cc97098e6aa88b57b712f41be9
Anton Radice <anton.radice@braincreators.com>
2020-02-13 17:16:10 +0100

    add cuda visible devices = 1 to all run scripts

--- run_snip_baselines.sh
+++ run_snip_baselines.sh
@@ -6,23 +6,19 @@
 # Run on cuda1 for now
 
 # LeNet300
-python main.py -m lenet300 -d mnist -bs 256 -e 100 -lr 1e-3 --prune_criterion snip \
+CUDA_VISIBLE_DEVICES=1 python main.py -m lenet300 -d mnist -bs 256 -e 100 -lr 1e-3 --prune_criterion snip \
                 --snip_sparsity 0.95 --opt adam --logdir="baselines" --comment="lenet300 snip=0.95" \
-                --device cuda:0
 
 # LeNet5
-python main.py -m lenet5 -d mnist -bs 256 -e 100 -lr 2e-3 --prune_criterion snip \
+CUDA_VISIBLE_DEVICES=1 python main.py -m lenet5 -d mnist -bs 256 -e 100 -lr 2e-3 --prune_criterion snip \
                 --snip_sparsity 0.95 --opt adam --logdir="baselines" --comment="lenet5 snip=0.95" \
-                --device cuda:0
 
 # ResNet18
-python main.py -m resnet18 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion snip \
+CUDA_VISIBLE_DEVICES=1 python main.py -m resnet18 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion snip \
                 --snip_sparsity 0.95 --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
                 --milestones 150 250 --logdir="baselines" --comment="resnet18 snip=0.95" \
-                --device cuda:0
 
 # VGG19
-python main.py -m vgg19 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion snip \
+CUDA_VISIBLE_DEVICES=1 python main.py -m vgg19 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion snip \
                 --snip_sparsity 0.95 --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
                  --milestones 150 250 --logdir="baselines" --comment="vgg19 snip=0.95" \
-                 --device cuda:0
--- run_unpruned_baselines.sh
+++ run_unpruned_baselines.sh
@@ -5,20 +5,20 @@
 # a baseline for comparing relative accuracy drop of pruned vs. unpruned.
 
 # LeNet300
-python main.py -m lenet300 -d mnist -bs 256 -e 100 -lr 1e-3 --prune_criterion none \
+CUDA_VISIBLE_DEVICES=1 python main.py -m lenet300 -d mnist -bs 256 -e 100 -lr 1e-3 --prune_criterion none \
                 --opt adam --logdir="baselines" --comment="lenet300 unrpuned"
 
 # LeNet5
-python main.py -m lenet5 -d mnist -bs 256 -e 100 -lr 2e-3 --prune_criterion none \
+CUDA_VISIBLE_DEVICES=1 python main.py -m lenet5 -d mnist -bs 256 -e 100 -lr 2e-3 --prune_criterion none \
                 --opt adam --logdir="baselines" --comment="lenet5 unpruned"
 
 # ResNet18
-python main.py -m resnet18 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion none \
+CUDA_VISIBLE_DEVICES=1 python main.py -m resnet18 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion none \
                 --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
                 --milestones 150 250 --logdir="baselines" --comment="resnet18 unpruned"
 
 # VGG19
-python main.py -m vgg19 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion none \
+CUDA_VISIBLE_DEVICES=1 python main.py -m vgg19 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion none \
                 --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
                  --milestones 150 250 --logdir="baselines" --comment="vgg19 unpruned"
 
