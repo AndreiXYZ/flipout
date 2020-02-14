208fba23516a8d7aa31ed3aaef1301a197337c36
Anton Radice <anton.radice@braincreators.com>
2020-02-14 18:17:16 +0100

    write some more run scripts

--- run_snip_baselines.sh
+++ run_snip_baselines.sh
@@ -22,3 +22,26 @@ CUDA_VISIBLE_DEVICES=1 python main.py -m resnet18 -d cifar10 -bs 128 -e 350 -lr 
 CUDA_VISIBLE_DEVICES=1 python main.py -m vgg19 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion snip \
                 --snip_sparsity 0.95 --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
                  --milestones 150 250 --logdir="baselines" --comment="vgg19 snip=0.95" \
+
+
+#############################################
+# Same thing but with snip=0.98             #
+#############################################
+
+# LeNet300
+CUDA_VISIBLE_DEVICES=1 python main.py -m lenet300 -d mnist -bs 256 -e 100 -lr 1e-3 --prune_criterion snip \
+                --snip_sparsity 0.98 --opt adam --logdir="baselines" --comment="lenet300 snip=0.99" \
+
+# LeNet5
+CUDA_VISIBLE_DEVICES=1 python main.py -m lenet5 -d mnist -bs 256 -e 100 -lr 2e-3 --prune_criterion snip \
+                --snip_sparsity 0.98 --opt adam --logdir="baselines" --comment="lenet5 snip=0.99" \
+
+# ResNet18
+CUDA_VISIBLE_DEVICES=1 python main.py -m resnet18 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion snip \
+                --snip_sparsity 0.98 --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
+                --milestones 150 250 --logdir="baselines" --comment="resnet18 snip=0.99" \
+
+# VGG19
+CUDA_VISIBLE_DEVICES=1 python main.py -m vgg19 -d cifar10 -bs 128 -e 350 -lr 0.1 --prune_criterion snip \
+                --snip_sparsity 0.98 --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
+                 --milestones 150 250 --logdir="baselines" --comment="vgg19 snip=0.99" \
