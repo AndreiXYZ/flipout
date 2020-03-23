57d33adc8eded6091c9942ce0bf83848134d2199
Anton Radice <anton.radice@braincreators.com>
2020-03-23 17:45:05 +0100

    add runfiles for vgg19

--- test_criterion_runfiles/run_noise_only_prunable.sh
+++ test_criterion_runfiles/run_noise_only_prunable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-device=1;
+device=0;
 seed=42;
 
 for prune_freq in 117 70 50 39; do
@@ -10,7 +10,7 @@ for prune_freq in 117 70 50 39; do
                 --milestones 150 250 --logdir="topflip_ablations" \
                 --comment="resnet18 crit=topflip pf=${prune_freq} seed=${seed}" \
                 --noise_only_prunable \
-                --save_model "pre-finetune/resnet18_topflip_pf${prune_freq}_s${seed}_noise_only_pruable"
+                --save_model "pre-finetune/resnet18_topflip_pf${prune_freq}_s${seed}_noise_only_prunable"
 
     # Use EMA flips
     CUDA_VISIBLE_DEVICES=${device} python main.py -m resnet18 -d cifar10 -bs 128 -tbs 10000 -e 350 -lr 0.1  \
@@ -18,8 +18,8 @@ for prune_freq in 117 70 50 39; do
                 --opt sgd --momentum 0.9 --noise --reg_type wdecay --lambda 5e-4 --use_scheduler \
                 --milestones 150 250 --logdir="topflip_ablations" \
                 --comment="resnet18 crit=topflip pf=${prune_freq} seed=${seed}" \
-                --use_ema_flips --beta_ema 0.9 \
-                --save_model "pre-finetune/resnet18_topflip_pf${prune_freq}_s${seed}_noise_only_pruable"
+                --use_ema_flips --beta_ema_flips 0.9 \
+                --save_model "pre-finetune/resnet18_topflip_pf${prune_freq}_s${seed}_ema_flips"
 
     # Use both
     CUDA_VISIBLE_DEVICES=${device} python main.py -m resnet18 -d cifar10 -bs 128 -tbs 10000 -e 350 -lr 0.1  \
@@ -27,7 +27,7 @@ for prune_freq in 117 70 50 39; do
                 --opt sgd --momentum 0.9 --noise --reg_type wdecay --lambda 5e-4 --use_scheduler \
                 --milestones 150 250 --logdir="topflip_ablations" \
                 --comment="resnet18 crit=topflip pf=${prune_freq} seed=${seed}" \
-                --use_ema_flips --beta_ema 0.9 --noise_only_prunable\
-                --save_model "pre-finetune/resnet18_topflip_pf${prune_freq}_s${seed}_noise_only_pruable"
+                --use_ema_flips --beta_ema_flips 0.9 --noise_only_prunable\
+                --save_model "pre-finetune/resnet18_topflip_pf${prune_freq}_s${seed}_noise_only_prunable_ema_flips"
 
 done
--- test_criterion_runfiles/seed43_gpu2.sh
+++ test_criterion_runfiles/seed43_gpu2.sh
@@ -12,34 +12,34 @@ sparsities=(
 for prune_freq in 117 70 50 39; do
     # Do magnitude and random
     for prune_criterion in 'global_magnitude' 'random';do
-        # ResNet18
-        CUDA_VISIBLE_DEVICES=${device} python main.py -m resnet18 -d cifar10 -bs 128 -tbs 10000 -e 350 -lr 0.1 \
+        # vgg19
+        CUDA_VISIBLE_DEVICES=${device} python main.py -m vgg19 -d cifar10 -bs 128 -tbs 10000 -e 350 -lr 0.1 \
                         --prune_criterion ${prune_criterion}  --prune_rate 0.5 --prune_freq  ${prune_freq} --seed ${seed} \
                         --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
-                        --milestones 150 250 --logdir="criterion_experiment_no_bias" \
-                        --comment="resnet18 crit=${prune_criterion} pf=${prune_freq} seed=${seed}" \
-                        --save_model "pre-finetune/resnet18_${prune_criterion}_pf${prune_freq}_s${seed}"
+                        --milestones 150 250 --logdir="criterion_experiment_no_bias/vgg19" \
+                        --comment="vgg19 crit=${prune_criterion} pf=${prune_freq} seed=${seed}" \
+                        --save_model "pre-finetune/vgg19_${prune_criterion}_pf${prune_freq}_s${seed}"
 
     done
     
     # Do flips
-    CUDA_VISIBLE_DEVICES=${device} python main.py -m resnet18 -d cifar10 -bs 128 -tbs 10000 -e 350 -lr 0.1  \
+    CUDA_VISIBLE_DEVICES=${device} python main.py -m vgg19 -d cifar10 -bs 128 -tbs 10000 -e 350 -lr 0.1  \
                 --prune_criterion topflip --prune_rate 0.5 --prune_freq ${prune_freq} --seed ${seed} \
                 --opt sgd --momentum 0.9 --noise --reg_type wdecay --lambda 5e-4 --use_scheduler \
-                --milestones 150 250 --logdir="criterion_experiment_no_bias" \
-                --comment="resnet18 crit=topflip pf=${prune_freq} seed=${seed}" \
-                --save_model "pre-finetune/resnet18_topflip_pf${prune_freq}_s${seed}"
+                --milestones 150 250 --logdir="criterion_experiment_no_bias/vgg19" \
+                --comment="vgg19 crit=topflip pf=${prune_freq} seed=${seed}" \
+                --save_model "pre-finetune/vgg19_topflip_pf${prune_freq}_s${seed}"
     
 done
 
 # Do SNIP
 for sparsity in ${sparsities[@]};do
-# ResNet18
-    CUDA_VISIBLE_DEVICES=${device} python main.py -m resnet18 -d cifar10 -bs 128 -e 350 -tbs 10000 -lr 0.1 \
+# vgg19
+    CUDA_VISIBLE_DEVICES=${device} python main.py -m vgg19 -d cifar10 -bs 128 -e 350 -tbs 10000 -lr 0.1 \
                     --prune_criterion snip --snip_sparsity ${sparsity} --seed ${seed} \
                     --opt sgd --momentum 0.9 --reg_type wdecay --lambda 5e-4 --use_scheduler \
-                    --milestones 150 250 --logdir="criterion_experiment_no_bias" \
-                    --comment="resnet18 crit=snip sparsity=${sparsity} seed=${seed}" \
-                    --save_model "pre-finetune/resnet18_snip_sp${sparsity}_s${seed}"
+                    --milestones 150 250 --logdir="criterion_experiment_no_bias/vgg19" \
+                    --comment="vgg19 crit=snip sparsity=${sparsity} seed=${seed}" \
+                    --save_model "pre-finetune/vgg19_snip_sp${sparsity}_s${seed}"
     
 done
