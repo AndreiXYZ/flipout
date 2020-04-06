c0cc54fcfe8fed342f1ac1311a5640bcee7b9dac
Andrei Apostol <andreia@test3.irc.sara.nl.irc.sara.nl>
2020-04-06 23:24:25 +0200

    add imagenette loaders (still need to do normalization)

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import PIL
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 
@@ -57,5 +58,47 @@ def cifar10_dataloaders(config):
                              num_workers = 8,
                              drop_last = False)
     
+
+    return train_loader, test_loader
+
+
+def image_loader(path):
+    img = Image.open(path)
+    return img
+
+def is_valid_file(path):
+    extension = path.split('.')[-1]
+    if extension == 'JPEG':
+        return True
+    return False
+
+def imagenette_dataloaders(config):
+    transforms_train = transforms.Compose([transforms.Resize((224,224)),
+                                           transforms.RandomCrop(224, padding=28),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor()])
+
+    transforms_test = transforms.Compose([transforms.Resize((224,224)),
+                                          transforms.ToTensor()])
+
+    train_set = datasets.DatasetFolder(root='./data/imagenette2/train', loader=image_loader,
+                                    is_valid_file=is_valid_file, transform=transforms_train)
+
+    test_set = datasets.DatasetFolder(root='./data/imagenette2/val', loader=image_loader,
+                                    is_valid_file=is_valid_file, transform=transforms_test)
+
+    train_loader = DataLoader(train_set, 
+                                batch_size = config['batch_size'],
+                                shuffle = True, 
+                                pin_memory = True,
+                                num_workers = 8,
+                                drop_last = False)
+
+    test_loader = DataLoader(test_set,
+                                batch_size = config['test_batch_size'],
+                                shuffle=False,
+                                pin_memory = True,
+                                num_workers = 8,
+                                drop_last = False)
 
     return train_loader, test_loader
