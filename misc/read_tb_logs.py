21660d6f2cbfd673d0a848404265a04bdd85362b
Andrei Apostol <andreia@test3.irc.sara.nl.irc.sara.nl>
2020-04-15 17:49:37 +0200

    improve regexes to accomodate hoyer square

--- misc/read_tb_logs.py
+++ misc/read_tb_logs.py
@@ -16,7 +16,7 @@ root_path = config['log_folder']
 # Prepare regexes
 re_model = re.compile(r' (\w+)[_| ]')
 re_seed = re.compile(r'seed=(.\w+)')
-re_crit = re.compile(r'crit=[a-z|_]+[_| ]')
+re_crit = re.compile(r'crit=.+[_| ]{1}')
 
 headers=['Model', 'Seed', 'Prune crit.', 'Sparsity']
 metrics = ['Test acc.']
@@ -29,8 +29,11 @@ for dirpath, dirs, files in os.walk(root_path):
         continue
     # Gather info about hparams
     model = re_model.search(dirpath).group(0)[:-1].strip()
-    seed = re_seed.search(dirpath).group(0).split('=')[1]
-    crit = re_crit.search(dirpath).group(0).split('=')[1][:-1]
+    seed = re_seed.search(dirpath).group(0).split('=')[1].strip()
+    crit = re_crit.search(dirpath).group(0).split('=')[1]
+    # Split from left to right by space or underscore, grab last elem
+    # then re-reverse string
+    crit = re.split('[ |_]', crit[::-1], maxsplit=1)[-1][::-1]
     
     # Open TB event file
     tb_event_file = os.path.join(dirpath, files[0])
@@ -90,9 +93,9 @@ for k, v in plot_dict.items():
     plt.xticks(np.arange(len(v['sparsities'])), truncated)
     break
 
-plt.title('Sparsity vs. acc (ResNet-18 Imagenette)')
+plt.title('Sparsity vs. acc (DenseNet-121 ImageNette)')
 plt.legend()
 plt.grid()
 plt.xlabel('Sparsity')
 plt.ylabel('Acc.')
-plt.savefig('./misc/' + 'resnet18_results.png')
+plt.savefig('./misc/' + 'densenet121_results.png')
